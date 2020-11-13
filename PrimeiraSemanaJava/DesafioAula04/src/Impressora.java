86bb9277cbdb4f0509e116435279d0659486c3f0
patriciasouzas <patriciasouzaa89@gmail.com>
2020-11-13 09:51:50 -0300

    correção classe Impressora

--- PrimeiraSemanaJava/DesafioAula04/src/Impressora.java
+++ PrimeiraSemanaJava/DesafioAula04/src/Impressora.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Impressora {
 
     public static void imprimaValor (String parametro) {
         System.out.println(parametro);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+         Impressora imprimir = new Impressora();
+    
+         imprimir.imprimaValor("Imprimir no console usando a classe Impressora deu certo!");
+    
     }
 
 }
